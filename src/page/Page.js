658fabdbb46ca8eecc7d470e722d0a297dd645e5
바키찬 <ckstmznf@naver.com>
2024-03-09 16:04:17 +0900

    기본 틀

--- src/page/Page.js
+++ src/page/Page.js
@@ -2,30 +2,70 @@ import styled from "styled-components";
 import {UI_ACTION_TYPE, useUiDispatch, useUiState} from "../context/UiReducer";
 import Button from "../component/Button";
 import {PageBasicStyle} from "../style/BasicStyle";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {LuImagePlus} from "react-icons/lu";
 import {IconButton} from "../component/IconButton";
 
 const PageStyle = styled.div`
   ${PageBasicStyle};
-  
-  background-color: #f00;
-  
+
   & > .content {
-    background-color: #0f0;
+
   }
 `
+
+function random(start, until) {
+  return Math.floor(Math.random() * 100000) % (until - start) + start
+}
+
+function randomChoice(array) {
+  return array[random(0, array.length)]
+}
 
 const Page = () => {
   const uiState = useUiState()
   const uiDispatch = useUiDispatch()
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState({
+    problemNum: null,
+    problemNumSystem: null,
+    answerNum: null,
+    answerNumSystem: null,
+  })
+
+  function init() {
+    let problemNumSystem, answerNumSystem
+    while (problemNumSystem === answerNumSystem) {
+      problemNumSystem = randomChoice([2, 8, 10, 16])
+      answerNumSystem = randomChoice([2, 8, 10, 16])
+    }
+
+    const problemNum_10 = random(2, 50)
+    let problemNum = problemNum_10.toString(problemNumSystem)
+    const answerNum = problemNum_10.toString(answerNumSystem)
+
+    setData({
+      problemNum,
+      problemNum_10,
+      problemNumSystem,
+      answerNum,
+      answerNumSystem,
+    })
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
 
   return (
     <PageStyle>
       <div className="content">
-        <Button onClick={() => uiDispatch({type:UI_ACTION_TYPE.modal_toggle})}>모달 토글</Button>
-        <IconButton onClick={() => {alert("Hello World")}}><LuImagePlus/></IconButton>
+        {data.problemNum}({data.problemNumSystem}) -> {data.answerNum}({data.answerNumSystem})
+
+        <br/>
+        {data.problemNum_10}
+
+        <Button onClick={() => init()}>클릭</Button>
       </div>
     </PageStyle>
   )
