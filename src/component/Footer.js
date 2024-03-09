99e4cb008913d8ca4858d18870780bcd85a596a5
바키찬 <ckstmznf@naver.com>
2024-03-09 19:44:32 +0900

    Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 import {useEffect, useRef} from "react";
 
 const FooterStyle = styled.footer`
+  background-color: ${p => p.theme.color.Gray4};
+
   & > div {
     width: 100%;
     height: 100%;
     max-width: ${p => p.theme.size.mobileMaxWidth}px;
-    
+
     padding: 12px;
     margin: 0 auto;
   }
@@ -23,7 +25,12 @@ const Footer = () => {
   return (
     <FooterStyle>
       <div ref={footerElement}>
-        푸터
+        <div>
+          <b>개발</b> : <a href={"https:kichan.dev"} target={"_blank"}>박희찬 (인천대학교 정보통신공학과 24학번)</a>
+        </div>
+        <div>
+          <b>오픈소스 프로젝트</b> : <a href="https://github.com/kichan05/jinbub" target={"_blank"}>깃허브</a>
+        </div>
       </div>
     </FooterStyle>
   )
