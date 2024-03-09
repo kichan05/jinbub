f3f2d94994feab337d070af3a46ee743de485730
바키찬 <ckstmznf@naver.com>
2024-03-09 20:39:42 +0900

    웹폰트 변경

--- src/style/Font.js
+++ src/style/Font.js
@@ -3,6 +3,5 @@ import {css} from "styled-components";
 export const FontStyle = css`
   @font-face {
     font-family: SUIT;
-    src: url("https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css");
   }
 `
